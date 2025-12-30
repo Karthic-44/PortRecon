cde2f654e097342841f55d3fc72c9ad8a7898df4
Karthic <karthics004@gmail.com>
2025-12-30 09:19:28 +0530

    added udp_scanner

---
+++ Modules/udp_scanner.py
@@ -0,0 +1,15 @@
+import socket 
+
+def scan_udp(host, port, timeout=1):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(timeout)
+        s.sendto(b"", (host, port))
+
+        try:
+            data, addr = s.recvfrom(1024)
+            return port
+        except socket.timeout:
+            return None
+    except:
+        return None
