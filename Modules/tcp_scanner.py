795704c2ef0af2a3a9defe11e6a7b8c1b9066fe7
Karthic <karthics004@gmail.com>
2025-12-30 12:06:56 +0530

    updated tcp_scanner

--- Modules/tcp_scanner.py
+++ Modules/tcp_scanner.py
@@ -1,10 +1,14 @@
 import socket 
 
-def scan_tcp_port(host, port, timeout=0.3):
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(timeout)
-            result = s.connect_ex((host, port))
-            return port if result == 0 else None
-    except:
-        return None
+def scan_tcp_port(host, ports, timeout=1):
+    open_ports = []
+    for port in ports:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(timeout)
+                result = s.connect_ex((host, port))
+                if result == 0:
+                    open_ports.append(port)
+        except:
+            pass
+    return open_ports
